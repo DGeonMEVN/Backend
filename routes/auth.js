3efebd6097aa8a3333e18f8a94c73e89cb393168
ovmkas <ovmkas@gmail.com>
2023-10-11 17:27:08 +0900

    redis accessToken, refreshToken 발급까지 완료

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares'); // 내가 만든 사용자 미들웨어
 const User = require('../models/user');
 const router = express.Router();
+const jwt = require('../utils/jwt-util');
+const redisClient = require('../utils/redisUtil');
 
 // * 회원 가입
 // 사용자 미들웨어 isNotLoggedIn을 통과해야 async (req, res, next) => 미들웨어 실행
@@ -56,12 +58,37 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
         if(!user) {
             return res.redirect(`/auth/?loginError=${info.message}`);
         }
+
+
         return req.login(user, loginError => {
             if(loginError) {
                 console.error(loginError);
                 return next(loginError);
             }
-            return res.send(user);
+
+            if (user) { // id, pw가 맞다면..
+                // access token과 refresh token을 발급합니다.
+                const accessToken = jwt.sign(user);
+                const refreshToken = jwt.refresh();
+
+                // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
+                redisClient.set(user.id, refreshToken);
+
+                res.status(200).json({ // client에게 토큰 모두를 반환합니다.
+                    ok: true,
+                    data: {
+                        accessToken,
+                        refreshToken,
+                    },
+                });
+            } else {
+                res.status(401).send({
+                    ok: false,
+                    message: 'password is incorrect',
+                });
+            }
+
+            // return res.send(user);
             // return res.redirect('/');
         });
     })(req, res, next);
