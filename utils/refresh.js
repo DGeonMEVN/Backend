cbe7eb731e2241414d5fc27c078334351b95fa5e
DGeon <ovmkas@gmail.com>
2023-10-26 18:56:51 +0900

    Vues에서 받아온 Header값들 검증 작업

--- utils/refresh.js
+++ utils/refresh.js
@@ -1,20 +1,24 @@
-const { sign, verify, refreshVerify } = require('../utils/jwt-util');
+const { sign, verify, refreshVerify } = require('./jwt-util');
 const jwt = require('jsonwebtoken');
 
 const refresh = async (req, res) => {
     // access token과 refresh token의 존재 유무를 체크합니다.
+    /* vuex의 access, refresh token 값을 받아와 확인 */
+    // authorization = accessToken / refresh = refreshToken
     if (req.headers.authorization && req.headers.refresh) {
         const authToken = req.headers.authorization.split('Bearer ')[1];
         const refreshToken = req.headers.refresh;
-
+        console.log('refresh.js authToken = ', authToken)
+        console.log('refresh.js refreshToken = ', refreshToken)
         // access token 검증 -> expired여야 함.
         const authResult = verify(authToken);
 
         // access token 디코딩하여 user의 정보를 가져옵니다.
         const decoded = jwt.decode(authToken);
-
+        console.log('refresh.js decoded = ', decoded)
         // 디코딩 결과가 없으면 권한이 없음을 응답.
         if (decoded === null) {
+            console.log('권한도 없음')
             res.status(401).send({
                 ok: false,
                 message: 'No authorized!',
@@ -23,24 +27,32 @@ const refresh = async (req, res) => {
 
         /* access token의 decoding 된 값에서
           유저의 id를 가져와 refresh token을 검증합니다. */
-        const refreshResult = refreshVerify(refreshToken, decoded.id);
-
+        console.log(decoded.userId)
+        const refreshResult = await refreshVerify(refreshToken, decoded.userId);
+        console.log('refreshResult = ', refreshResult)
+        console.log('refreshResult = ', refreshResult.ok)
         // 재발급을 위해서는 access token이 만료되어 있어야합니다.
         if (authResult.ok === false && authResult.message === 'jwt expired') {
             // 1. access token이 만료되고, refresh token도 만료 된 경우 => 새로 로그인해야합니다.
-            if (refreshResult.ok === false) {
-                res.status(401).send({
+            if (!refreshResult) {
+                console.log('refresh.js 통과 됐니?')
+                res.json({
                     ok: false,
                     message: 'No authorized!',
                 });
             } else {
                 // 2. access token이 만료되고, refresh token은 만료되지 않은 경우 => 새로운 access token을 발급
-                const newAccessToken = sign(user);
+                const member = {
+                    userId: decoded.userId,
+                }
+                console.log('refresh.js member = ', member)
+                const newAccessToken = sign(member);
+                console.log('refresh.js newAccessToken = ', newAccessToken)
 
-                res.status(200).send({ // 새로 발급한 access token과 원래 있던 refresh token 모두 클라이언트에게 반환합니다.
+                res.status(200).json({ // 새로 발급한 access token과 원래 있던 refresh token 모두 클라이언트에게 반환합니다.
                     ok: true,
                     data: {
-                        accessToken: newAccessToken,
+                        accessToken: `Bearer ${newAccessToken}`,
                         refreshToken,
                     },
                 });
@@ -53,11 +65,13 @@ const refresh = async (req, res) => {
             });
         }
     } else { // access token 또는 refresh token이 헤더에 없는 경우
-        res.status(400).send({
+        console.log('비로그인 상태(토큰값 없음)')
+        res.status(403).send({
             ok: false,
             message: 'Access token and refresh token are need for refresh!',
         });
     }
 };
+
 
 module.exports = refresh;
