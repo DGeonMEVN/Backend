745a3dff93cc4d9b678bd6bbfbd63691143b605a
DGeon <ovmkas@gmail.com>
2023-10-27 14:08:39 +0900

    authJWT 검증 후 , USER데이터 조회 후 json으로 Vue에 전달

--- utils/authJWT.js
+++ utils/authJWT.js
@@ -1,14 +1,16 @@
 const { verify } = require('./jwt-util');
 
 const authJWT = (req, res, next) => {
+    // console.log("authJWT req.headers.authorization = " , req.headers.authorization);
     if (req.headers.authorization) {
         const token = req.headers.authorization.split('Bearer ') [1]; // header에서 access token을 가져옵니다.
         const result = verify(token); // token을 검증합니다.
         if (result.ok) { // token이 검증되었으면 req에 값을 세팅하고, 다음 콜백함수로 갑니다.
-            req.id = result.id;
-            req.role = result.role;
+            req.userId = result.userId;
+            console.log("authJWT result.ok = 통과" , req.userId);
             next();
         } else { // 검증에 실패하거나 토큰이 만료되었다면 클라이언트에게 메세지를 담아서 응답합니다.
+            console.log("authJWT result.ok = 미통과" );
             res.status(401).send({
                 ok: false,
                 message: result.message, // jwt가 만료되었다면 메세지는 'jwt expired'입니다.
